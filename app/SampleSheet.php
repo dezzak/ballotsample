06ae0baa0b7ca87f0e2d120e1459764b0fede0cc
Derek Kaye <github@dezzanet.co.uk>
2017-05-14 16:11:40 +0100

    Update SampleSheet to use new interface and not be tied to 3rd party library

--- app/SampleSheet.php
+++ app/SampleSheet.php
@@ -2,7 +2,9 @@
 
 namespace Dezzak\BallotSample;
 
-class SampleSheet extends \FPDF
+use Dezzak\BallotSample\Output\PDFInterface;
+
+class SampleSheet
 {
     const FONT_FACE = 'Times';
     /** @var Poll */
@@ -15,6 +17,13 @@ class SampleSheet extends \FPDF
     private $sampleHeight;
     /** @var int */
     private $sampleCount;
+    /** @var PDFInterface */
+    private $pdf;
+
+    public function __construct(PDFInterface $pdf)
+    {
+        $this->pdf = $pdf;
+    }
 
     /** @return Poll */
     public function getPoll()
@@ -29,25 +38,25 @@ class SampleSheet extends \FPDF
 
     public function generate()
     {
-        $this->AddPage();
-        $this->setFont(self::FONT_FACE, '', 12);
+        $this->pdf->addPage();
+        $this->pdf->setFont(self::FONT_FACE, '', 12);
 
         $this->generateHeader();
-        while (($this->GetY() + $this->sampleHeight) < 280) {
+        while (($this->pdf->getY() + $this->sampleHeight) < 280) {
             $this->generateBoxInfo();
             $this->generateCandidateSamples();
             if (!$this->sampleHeight) {
-                $this->sampleHeight = $this->GetY() - $this->endOfHeader;
+                $this->sampleHeight = $this->pdf->getY() - $this->endOfHeader;
             }
         }
     }
 
     private function generateHeader()
     {
-        $this->setFont(self::FONT_FACE, 'B', 15);
-        $this->Cell(190, 10, $this->poll->getDescription(), 1, 0, 'C');
-        $this->Ln();
-        $this->endOfHeader = $this->GetY();
+        $this->pdf->setFont(self::FONT_FACE, 'B', 15);
+        $this->pdf->addCell(190, 10, $this->poll->getDescription(), 1, 0, 'C');
+        $this->pdf->addLine();
+        $this->endOfHeader = $this->pdf->getY();
     }
 
     private function generateCandidateSamples()
@@ -61,47 +70,47 @@ class SampleSheet extends \FPDF
     {
         $name = strtoupper($candidate->getSurname()) . ', ' . explode(' ', $candidate->getFirstNames())[0];
 
-        $this->setFont(self::FONT_FACE, '', 12);
+        $this->pdf->setFont(self::FONT_FACE, '', 12);
         $infoWidth = 55;
-        $preImageX = $this->GetX();
-        $preImageY = $this->GetY();
-        $this->Image($candidate->getParty()->getLogoPath(), null, null, 9, 9);
-        $this->setXY($preImageX, $preImageY);
-        $this->Cell(9, 9, '', 1);
-        $this->Cell(46, 9, $name, 1);
+        $preImageX = $this->pdf->getX();
+        $preImageY = $this->pdf->getY();
+        $this->pdf->addImage($candidate->getParty()->getLogoPath(), 9, 9);
+        $this->pdf->setXY($preImageX, $preImageY);
+        $this->pdf->addCell(9, 9, '', 1);
+        $this->pdf->addCell(46, 9, $name, 1);
 
         $this->sampleCount = 1;
         $this->generateSampleLine();
-        $this->Cell($infoWidth);
+        $this->pdf->addCell($infoWidth);
         $this->generateSampleLine();
-        $this->Cell($infoWidth);
+        $this->pdf->addCell($infoWidth);
         $this->generateSampleLine();
-        $this->Ln();
+        $this->pdf->addLine();
     }
 
     private function generateSampleLine()
     {
-        $this->SetFont(self::FONT_FACE, '', 6);
-        $this->SetDrawColor(0x99);
+        $this->pdf->setFont(self::FONT_FACE, '', 6);
+        $this->pdf->setDrawColour(0x99, 0x99, 0x99);
         for ($i = 1; $i <= 45; ++$i) {
-            $this->Cell(3, 3, ($this->sampleCount % 100), 1, 0, 'C');
+            $this->pdf->addCell(3, 3, ($this->sampleCount % 100), 1, 0, 'C');
             $this->sampleCount++;
         }
-        $this->SetDrawColor(0);
-        $this->Ln();
+        $this->pdf->SetDrawColour(0, 0, 0);
+        $this->pdf->addLine();
     }
 
     private function generateBoxInfo()
     {
-        $this->setFont(self::FONT_FACE, 'B', 10);
+        $this->pdf->setFont(self::FONT_FACE, 'B', 10);
         $lineHeight = 8;
-        $this->Cell(25, $lineHeight, 'Box Location');
-        $this->Cell(165, $lineHeight, '', 'B');
-        $this->Ln();
-        $this->Cell(25, $lineHeight, 'Box Number');
-        $this->Cell(70, $lineHeight, '', 'B');
-        $this->Cell(25, $lineHeight, 'Polling District');
-        $this->Cell(70, $lineHeight, '', 'B');
-        $this->Ln(12);
+        $this->pdf->addCell(25, $lineHeight, 'Box Location');
+        $this->pdf->addCell(165, $lineHeight, '', 'B');
+        $this->pdf->addLine();
+        $this->pdf->addCell(25, $lineHeight, 'Box Number');
+        $this->pdf->addCell(70, $lineHeight, '', 'B');
+        $this->pdf->addCell(25, $lineHeight, 'Polling District');
+        $this->pdf->addCell(70, $lineHeight, '', 'B');
+        $this->pdf->addLine(12);
     }
 }
